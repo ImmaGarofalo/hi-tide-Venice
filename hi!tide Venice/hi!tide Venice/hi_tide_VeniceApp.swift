2ef54e91139f5c5ced8477a12b03d99e5e006a8a
ImmaGarofalo <igarofalo22@fed.idserve.net>
2022-11-17 12:38:50 +0100

    Project created 1

---
+++ hi!tide Venice/hi!tide Venice/hi_tide_VeniceApp.swift
@@ -0,0 +1,17 @@
+//
+//  hi_tide_VeniceApp.swift
+//  hi!tide Venice
+//
+//  Created by MacProImma on 17/11/22.
+//
+
+import SwiftUI
+
+@main
+struct hi_tide_VeniceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
